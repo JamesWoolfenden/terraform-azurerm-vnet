e393040b63aecd4d5c52b68cd2ec6b7d53ea1a21
James Woolfenden <James.Woolfenden@gmail.com>
2021-07-01 08:54:39 +0100

    refresh

--- example/examplea/terraform.tf
+++ example/examplea/terraform.tf
@@ -1,16 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-      version = "2.59.0"
+      version = "2.65.0"
       source  = "hashicorp/azurerm"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "2.0.0"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "2.2.0"
     }
   }
   required_version = ">=0.14.8"
